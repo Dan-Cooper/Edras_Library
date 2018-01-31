551596db2073b1bae569f86e7f7d542f66eca222
DanCooper <dannycooper1011@gmail.com>
2018-01-31 15:17:55 -0500

    Added Simple Player
    Player moves and cam rotates

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
-public class PlayerControler : MonoBehaviour {
+public class PlayerControler : MonoBehaviour
+{
+	private CharacterController charControl;
+	public float walkSpeed;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake()
+	{
+		charControl = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		MovePlayer();
+	}
+
+	void MovePlayer()
+	{
+		//Gets Raw input data
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		//Modifies data for speed and direction
+		Vector3 moveSide = transform.right * h * walkSpeed;
+		Vector3 moveForw = transform.forward * v * walkSpeed;
+		//Makes change to object
+		charControl.SimpleMove(moveSide);
+		charControl.SimpleMove(moveForw);
+
 	}
 }
