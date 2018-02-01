0662e2932b97720c9fe160694290c2f371437318
Cathy Nguyen <Smileycathy1128@users.noreply.github.com>
2018-01-31 19:39:30 -0500

    I added the MovePlatform script
    I'll remove it if it causes issues.

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour {
+	private Rigidbody rb;
+	public Rigidbody rbEnd;	// EndPlatform will move with parent gameobject after Start function
+
+	public float speed;
+	private Vector3 start;
+	private Vector3 end;
+	private Vector3 moveVector;
+	private bool goingBack;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+
+		start = rb.position;
+		end = rbEnd.position;	// only EndPlatform's initial position is stored.
+		moveVector = end - start;
+
+		goingBack = false;
+	}
+
+	void FixedUpdate () {
+		if(!goingBack) {
+			rb.velocity = moveVector.normalized * speed;
+			if (rb.position == end) {				// only works if no decimals
+				goingBack = true;
+			}
+		}
+		else if(goingBack) {
+			rb.velocity = -moveVector.normalized * speed;
+			if(rb.position == start) {
+				goingBack = false;
+			}
+		}
+	}
+}
