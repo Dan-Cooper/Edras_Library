ed3bbfa098115c27f4f55f6056d9ab6f34d9dc1a
Cathy Nguyen <Smileycathy1128@users.noreply.github.com>
2018-02-06 00:24:14 -0500

    Update ramp spawning script

--- Assets/Scripts/RampSpawning.cs
+++ Assets/Scripts/RampSpawning.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RampSpawning : MonoBehaviour {
+	public GameObject ramp;
+	public Transform playerTransform;
+	public int maxRamps;
+	[SerializeField]
+	private int currentRamps;
 
-	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetButton("Fire3") && (currentRamps < maxRamps)){
+			Debug.Log("get button successs!");
+			Vector3 rampSpawnPoint = new Vector3(0f, 0f, 3f);
+
+			Instantiate(ramp, rampSpawnPoint,Quaternion.identity, playerTransform);
+			currentRamps += 1;
+		}
 	}
 }
