b58c9ae6ef3616cea3a9eff1dd714bfd01fdf5ff
DanCooper <dannycooper1011@gmail.com>
2018-02-07 18:27:35 -0500

    Started Wall climb Detection
    Casting rays and debug info. 
    
    Forward detection working. //TODO add functionality

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -22,6 +22,11 @@ public class PlayerControler : MonoBehaviour
 	private bool jumpRequest;
 	private Vector3 moveDir;
 
+
+	private bool fDetect;
+	private bool lDetect;
+	private bool rdetect;
+
 	// Use this for initialization
 	void Awake()
 	{
@@ -30,11 +35,14 @@ public class PlayerControler : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		ParcoreDetetion();
 		
 		if (Input.GetButtonDown("Jump") && charControl.isGrounded) jumpRequest = true;
 		if (Input.GetButtonDown("Run")) walkSpeed *= 2;
 		if (Input.GetButtonUp("Run")) walkSpeed /= 2;
+		
 		MovePlayer();
 	}
 /* Was used in simplemove might bring back.
@@ -60,21 +68,57 @@ public class PlayerControler : MonoBehaviour
 			{
 				moveDir.y = jumpVelocity;
 				jumpRequest = false;
-				
+
 			}
+			fDetect = false;
 		}
-		if (moveDir.y < 0.2)
+		if (moveDir.y < 0.2 && fDetect)
 		{
 			moveDir += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-			//Debug.Log("Long" + moveDir.y);
+			Debug.Log("Long" + fDetect);
 		}
 		else if (moveDir.y > 0.2 && !Input.GetButton("Jump"))
 		{
 			moveDir += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-			//Debug.Log("short" + moveDir.y);
+			Debug.Log("short"+ fDetect);
 		}
 		//Debug.Log("Move" + moveDir);
 		moveDir.y -= gravity * Time.deltaTime;
 		charControl.Move(moveDir * Time.deltaTime);
+	}
+	
+	void WallClimb()
+	{
+	    
+	}
+
+	//Fires Raycasts around the player at center and top of prefab.
+	void ParcoreDetetion()
+	{
+		Vector3 mFwd = transform.TransformDirection(Vector3.forward);//Forword DIR
+		Ray rMfRay = new Ray(transform.position, mFwd);
+		Ray rHfRay = new Ray(transform.position+ new Vector3(0,1.5f,0), mFwd);
+
+		if (Physics.Raycast(rHfRay, 1)) fDetect = true;
+
+		Vector3 mLft = transform.TransformDirection(Vector3.left);//Left DIR
+		Ray rMlRay = new Ray(transform.position, mLft);
+		Ray rHlRay = new Ray(transform.position+ new Vector3(0,1.5f,0), mLft);
+		
+		Vector3 mRgt = transform.TransformDirection(Vector3.right);//Right DIR
+		Ray rMrRay = new Ray(transform.position, mRgt);
+		Ray rHrRay = new Ray(transform.position+ new Vector3(0,1.5f,0), mRgt);
+
+		if (true)//Set to True to show Debug Rays
+		{
+			Debug.DrawRay(transform.position, mFwd, Color.yellow);//Mid Forword
+			Debug.DrawRay(transform.position+ new Vector3(0,1.5f,0), mFwd, Color.yellow);//High Forword
+			
+			Debug.DrawRay(transform.position, mLft, Color.red);//Mid Left
+			Debug.DrawRay(transform.position+ new Vector3(0,1.5f,0), mLft, Color.red);//High Left
+			
+			Debug.DrawRay(transform.position, mRgt, Color.green);//Mid Right
+			Debug.DrawRay(transform.position+ new Vector3(0,1.5f,0), mRgt, Color.green);//High Right
+		}
 	}
 }
