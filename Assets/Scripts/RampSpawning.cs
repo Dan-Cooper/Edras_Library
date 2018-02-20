8a893beff82f34a7f815831c5a5e347b1179e900
Cathy Nguyen <Smileycathy1128@gmail.com>
2018-02-20 14:15:14 -0500

    Push
    I clean the ramp spawning script.

--- Assets/Scripts/RampSpawning.cs
+++ Assets/Scripts/RampSpawning.cs
@@ -5,65 +5,104 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RampSpawning : MonoBehaviour {
-	public GameObject ramp;
-	public GameObject rampGuideObj;
-	public Transform playerTransform;
-	public Transform cameraTransform;	//unused?
-	public Transform whereRampSpawns;
+
 	public int maxRamps;
 
+	[Space]
+
+	[Header("0 = spawn ramp")]
+	[Header("1 = spawn floor")]
+	[Header("2 = spawn wall")]
+	[Header("3 = spawn lift")]
+	[Header("any other # = error")]
+	[Header("Size should be 4.")]
+	public GameObject[] ramp;
+	public GameObject[] rampGuideObj;
+
+	[Space]
+
+	public Transform playerTransform;
+	public Transform whereRampSpawns;
 	private GameObject guideInst;
+	//	public Transform cameraTransform;	//unused?
 
-//	public int rampTag;
-	/*	0 = platform spawning disabled
-	 *	1 = spawn ramp
-	 *	2 = spawn floor
-	 *	3 = spawn wall
-	 *	4 = spawn lift
-	 *	any other # = error
-	*/
+	[Space]
 
-	[SerializeField]
-	private int currentRamps;
-	[SerializeField]
-	private bool prepareRamp;
+	[Header("Variables below used just for")]
+	[Header("    reference")]	// They will be reset at Start()
+	public bool rampEnable;
+	[SerializeField]	private bool prepareRamp;
+	[SerializeField]	private int rampTag;	// for the array
+	[SerializeField]	private int currentRamps;
+
+
 
 	void Start () {
+		rampEnable = true;
 		prepareRamp = false;
+		rampTag = 0;
+		currentRamps = 0;
 	}
 	
 	void Update () {
-		if (Input.GetButtonDown("Summon")){
+		if(rampEnable){
 
-			if (prepareRamp) {
-				// then delete rampGuide instance
-				Destroy(guideInst);
-//				rampInst =
-				Instantiate(ramp
-					, whereRampSpawns.position
-					, playerTransform.rotation
-				);
-
-				prepareRamp = false;
-				Debug.Log("Is prepareRamp false? " + prepareRamp);
+			if(Input.GetButtonDown("Switch Plat")){
+				// assign ramp# based on rampTag
+				if(rampTag>=0 && rampTag <=3){
+					rampTag +=1;
+				}
+				else if(rampTag == 4){
+					rampTag = 0;
+				}
 			}
-			else if((currentRamps < maxRamps) && !prepareRamp){
-				Debug.Log("get button successs!");
-				guideInst = 
-				Instantiate(rampGuideObj
-					, whereRampSpawns.position
-					, playerTransform.rotation
-					, playerTransform
-				);
+				
+			if (Input.GetButtonDown("Summon Plat")){
 
-				currentRamps += 1;
-				prepareRamp = true;
-				Debug.Log("Is prepareRamp true? " + prepareRamp);
+		
+				if (prepareRamp) {
+
+					Destroy(guideInst);	// deletes rampGuide instance
+	//				rampInst =
+
+					//	if not cancelled or switched
+					Instantiate(ramp[rampTag]
+						, whereRampSpawns.position
+						, playerTransform.rotation
+					);
+					// "if switched" go here
+					// "if cancellled" go here
+
+					prepareRamp = false;
+					Debug.Log("Is prepareRamp false? " + prepareRamp);
+				}
+				else if((currentRamps < maxRamps) && !prepareRamp){
+					Debug.Log("get button successs!");
+					guideInst = 
+					Instantiate(rampGuideObj[rampTag]
+						, whereRampSpawns.position
+						, playerTransform.rotation
+						, playerTransform
+					);
+
+					currentRamps += 1;
+					prepareRamp = true;
+					Debug.Log("Is prepareRamp true? " + prepareRamp);
+				}
+
+				else {
+					Debug.Log("Some error.");
+				}
+
 			}
-
-
 
 		}
+
+		else {	//	When ramp spawning is disabled.
+			Debug.Log("Ramp spawning disabled. :3");
+		}
+
+
 
 
 	}
