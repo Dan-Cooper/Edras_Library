04fe435868281505ef9277d3d8e74b05d4446549
Cathy Nguyen <Smileycathy1128@gmail.com>
2018-03-27 01:58:23 -0400

    Updated a script

--- Assets/Scripts/LeverSwitchButtonScript.cs
+++ Assets/Scripts/LeverSwitchButtonScript.cs
@@ -5,19 +5,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LeverSwitchButtonScript : MonoBehaviour {
-	public bool isOn;
-	public bool triggeredByPlayerInput;
+	public bool singleUse;
+	public bool playerInputRequired;
+
+	[HideInInspector] public bool isOn;
+	[HideInInspector] public bool isEnabled;
+
+//	public Vector3 playerLocation;
+
+//	void Start() {
+//		GameObject player = GameObject.FindGameObjectsWithTag("Player");
+//		playerLocation = GetComponent<Transform>();
+//	}
 
 	void OnTriggerStay (Collider other) {		// when the player is close by
-		if(triggeredByPlayerInput){
+		if(playerInputRequired){
+			if(isEnabled && Input.GetButtonDown("Submit")){
+				isOn = true;
+				if(singleUse){
+					enabled = false;
+				}
+			}
 			// check if player is facing switch
 
 		}
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if(!triggeredByPlayerInput){
-			isOn = !isOn;
+		if(isEnabled && !playerInputRequired){
+			isOn = true;
+			if(singleUse){
+				enabled = false;
+			}
 		}
 	}
 }
