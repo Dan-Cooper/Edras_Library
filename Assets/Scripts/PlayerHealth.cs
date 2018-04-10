1276bea554b5eb9dea1ce9c8ff1f6b732f80d246
Cathy Nguyen <Smileycathy1128@gmail.com>
2018-04-10 01:39:01 -0400

    Added Player Health Script

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,23 @@
+﻿// Cathy
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+	public Text m_health;
+	private PlayerControler playCon;
+
+	// Use this for initialization
+	void Start () {
+		GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+		playCon = playerObj.GetComponent<PlayerControler>();
+		// m_health.text = "Health: " + playCon.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
