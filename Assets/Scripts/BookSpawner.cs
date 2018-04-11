c297072f053149cfea9dc8333c2e97914cb299e8
DanCooper <dannycooper1011@gmail.com>
2018-04-10 22:36:39 -0400

    Started Spawner
    Book spawner now spawning boxes.
    TODO Delay spawning.

---
+++ Assets/Scripts/BookSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class BookSpawner : MonoBehaviour
+{
+
+	public GameObject Book;
+
+	public float Speed;
+
+
+
+	private bool _spawn;
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(Timer());
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if(_spawn)BookSpawn();
+	}
+
+	void BookSpawn()
+	{
+		GameObject book = Instantiate(Book, transform.position, transform.rotation);
+		book.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * Speed);
+	}
+	
+	IEnumerator Timer ()
+	{   
+		print("Reached the target.");
+		_spawn = false;
+		yield return new WaitForSeconds(1f);
+		_spawn = true;
+        
+		print("routine is now finished.");
+	}
+}
