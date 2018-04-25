8d6c62fa818c994035374d654bd34cfce2b77837
Cathy Nguyen <Smileycathy1128@gmail.com>
2018-04-25 14:56:09 -0400

    Organized RampSpawning script.
    Tweaked minDIstance on Player prefab inspector.

--- Assets/Scripts/RampSpawning.cs
+++ Assets/Scripts/RampSpawning.cs
@@ -8,146 +8,141 @@ public class RampSpawning : MonoBehaviour {
 
 	public float minDistance = 3f;
 	public float maxDistance = 15f;
-
 	[Space]
-
 	[Header("Magic Platform Limits:")]
 	public bool enableMaxByType = false;
 	public int[] maxByType;
+	[Space]
 	public bool enableMaxTotal = true;
 	public int maxTotal = 15;
-
 	[Header("Size should be the same integars.")]
 	public int arraySize;
 	public GameObject[] ramp;
 	public GameObject[] rampGuideObj;
-
 	[Space]
-
 	public Transform whereRampSpawns;	// Why 2 transforms?
 	private Transform playerTransform;	// Why 2 transforms?
 	private GameObject guideInst;
-
 	[Space]
-
-//	[Header("Variables below used for enabling")]
-//	[Header("    and disabling magic platform ")]
-//	[Header("    spawning")]			// They will be reset at Start()
 	[HideInInspector] public bool rampEnable;
-//	[SerializeField]	
 	private bool prepareRamp;
-//	[SerializeField]	
-	private int rampTag;	// for the array
-//	[SerializeField]	
-	private int currentRamps;
+	private int rampTag;	// for the arrays
+	private int currentRampTotal;
+//##############################	public int[] currentRampByType;
+
 
 	void Start () {
 		playerTransform = GetComponent<Transform>();
 		rampEnable = true;
 		prepareRamp = false;
 		rampTag = 0;
-		currentRamps = 0;
+		currentRampTotal = 0;
 	}
-
-	
+		
 	void Update () {
-		if(rampEnable){
-			//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+		if(rampEnable){		
 			// Inputs: "Summon Plat", "Switch Plat", "Mouse ScrollWheel", ""
 			if (Input.GetButtonDown("Summon Plat")){
 
-		
 				if (prepareRamp) {
-
-					Destroy(guideInst);	// deletes rampGuide instance
-
-					Instantiate(ramp[rampTag]
-						, guideInst.transform.position
-						, playerTransform.rotation
-					);
-												// Ctrl + F add sound here
-					prepareRamp = false;
+					
+					SummonPlatMethod();
 				}
-				else if((currentRamps < maxTotal) && !prepareRamp){
-					guideInst = 
-						Instantiate(rampGuideObj[rampTag]
-							, whereRampSpawns.position
-							, playerTransform.rotation
-							, playerTransform
-//							, whereRampSpawns
-						);
-												// Ctrl + F add sound here
-
-					currentRamps += 1;
-					prepareRamp = true;
+				else if((currentRampTotal < maxTotal) && !prepareRamp){
+					SummonGuideMethod();
 				}
-
 				else {
 					Debug.Log("CN: Some ramp spawning error.");
 				}
-
 			}
+			SwitchPlatMethod();
 
-			if(Input.GetButtonDown("Switch Plat")){
-				// assign ramp# based on rampTag
-				if(rampTag>=0 && rampTag < arraySize-1){
-					rampTag +=1;
-				}
-				else if(rampTag == arraySize-1){
-					rampTag = 0;
-				}
-				else{
-					Debug.Log("Error.");
-				}
-
-				if(prepareRamp){
-					Destroy(guideInst);
-					guideInst =
-						Instantiate(rampGuideObj[rampTag]
-							, whereRampSpawns.position
-							, playerTransform.rotation
-							, playerTransform
-//							,whereRampSpawns
-						);
-												// Ctrl + F add sound here
-				}
-			}
-			// #################################"Mouse ScrollWheel"
 			if(prepareRamp){
-				Debug.Log(guideInst.transform.localPosition.y);
-
-				guideInst.transform.localPosition += new Vector3(0f, 0f,	// scrolling thing
-					Input.GetAxis("Mouse ScrollWheel")*10f);
-				
-				if(guideInst.transform.localPosition.z <= minDistance){		// minimum distance from player
-					guideInst.transform.localPosition = 
-						new Vector3(0f,whereRampSpawns.position.y,minDistance);
-				}
-				if(guideInst.transform.localPosition.z >= maxDistance){		// maximum distance from player
-					guideInst.transform.localPosition = 
-						new Vector3(0f,whereRampSpawns.position.y,maxDistance);
-				}
-			// ################################
-
-
+				ScrollPlatMethod();
+		
 				if(Input.GetButtonDown("Undo Summon")){
 					Destroy(guideInst);
 					prepareRamp = false;
 				}
-
 			}
-
 		}
-
-		else {	//	When ramp spawning is disabled.
+		else {	//	When disabled
 			Debug.Log("CN: Ramp spawning disabled. :3");
 		}
 
+	}
+	//####################################################################
 
+	void SummonPlatMethod() {
+		Destroy(guideInst);	// deletes rampGuide instance
 
-
+		Instantiate(ramp[rampTag]
+			, guideInst.transform.position
+			, playerTransform.rotation
+		);
+		// Ctrl + F add sound here
+		prepareRamp = false;
 	}
 
+	void SummonGuideMethod() {
+		guideInst = 
+			Instantiate(rampGuideObj[rampTag]
+				, whereRampSpawns.position
+				, playerTransform.rotation
+				, playerTransform
+				//							, whereRampSpawns
+			);
+		// Ctrl + F add sound here
+
+		currentRampTotal += 1;
+		prepareRamp = true;
+	}
+	//####################################################################
+
+	void SwitchPlatMethod() {
+		if(Input.GetButtonDown("Switch Plat")){
+			// assign ramp# based on rampTag
+			if(rampTag>=0 && rampTag < arraySize-1){
+				rampTag +=1;
+			}
+			else if(rampTag == arraySize-1){
+				rampTag = 0;
+			}
+			else{
+				Debug.Log("Error.");
+			}
+
+			if(prepareRamp){
+				Destroy(guideInst);
+				guideInst =
+					Instantiate(rampGuideObj[rampTag]
+						, whereRampSpawns.position
+						, playerTransform.rotation
+						, playerTransform
+						//							,whereRampSpawns
+					);
+				// Ctrl + F add sound here
+			}
+		}
+	}
+	//####################################################################
+
+	void ScrollPlatMethod() {
+		Debug.Log(guideInst.transform.localPosition.y);
+
+		guideInst.transform.localPosition += new Vector3(0f, 0f,	// scrolling thing
+			Input.GetAxis("Mouse ScrollWheel")*10f);
+
+		if(guideInst.transform.localPosition.z <= minDistance){		// minimum distance from player
+			guideInst.transform.localPosition = 
+				new Vector3(0f,whereRampSpawns.position.y,minDistance);
+		}
+		if(guideInst.transform.localPosition.z >= maxDistance){		// maximum distance from player
+			guideInst.transform.localPosition = 
+				new Vector3(0f,whereRampSpawns.position.y,maxDistance);
+		}
+	}
+	//####################################################################
 
 
 }
