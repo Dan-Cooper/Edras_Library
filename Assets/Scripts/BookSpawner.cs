83205e9ad32b0eb1e4bb55ed2e0f11ee010251c6
Quintin <qconnell@student.fitchburgstate.edu>
2018-05-08 19:16:19 -0400

    Added animated books to Spawner, modified code for random book colors

--- Assets/Scripts/BookSpawner.cs
+++ Assets/Scripts/BookSpawner.cs
@@ -8,23 +8,36 @@ public class BookSpawner : MonoBehaviour
 
 	public GameObject Book;
 
+	public GameObject Book2;
+
+	public GameObject Book3;
+
 	public float Speed;
 
-
-
 	private bool _spawn;
-	
+
+	private Random rand = new Random();
+	private float ranumb = 1;
 
 	// Use this for initialization
 	void Start ()
 	{
 		StartCoroutine(Timer());
+
 	}
 
 	void BookSpawn()
 	{
-		GameObject book = Instantiate(Book, transform.position, transform.rotation);
-		book.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * Speed);
+		if (ranumb >= 0.3) {
+			GameObject book = Instantiate (Book, transform.position, transform.rotation);
+			book.GetComponent<Rigidbody> ().velocity = transform.TransformDirection (Vector3.forward * Speed);
+		} else if (ranumb >= 0.6){
+			GameObject book = Instantiate (Book2, transform.position, transform.rotation);
+			book.GetComponent<Rigidbody> ().velocity = transform.TransformDirection (Vector3.forward * Speed);
+		} else if (ranumb < 0.6){
+			GameObject book = Instantiate (Book3, transform.position, transform.rotation);
+			book.GetComponent<Rigidbody> ().velocity = transform.TransformDirection (Vector3.forward * Speed);
+		}
 	}
 	
 	IEnumerator Timer ()
@@ -34,6 +47,7 @@ public class BookSpawner : MonoBehaviour
 		{
 			_spawn = false;
 			yield return new WaitForSeconds(1f);
+			ranumb = Random.value;
 			BookSpawn();
 			_spawn = true;
 			yield return new WaitForSeconds(1f);
