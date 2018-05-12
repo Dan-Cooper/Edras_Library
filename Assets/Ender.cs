ab3ff2f5ec49af0f2cffc3fae8a7245f9eae141a
Quintin <qconnell@student.fitchburgstate.edu>
2018-05-12 12:32:13 -0400

    Added an "End Book" That ends the game when the player approaches

---
+++ Assets/Ender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ender : MonoBehaviour {
+	public GameObject BlackScreen;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			GetComponent<Animator> ().SetTrigger ("Player");
+			StartCoroutine (Fade ());
+			other.GetComponent<PlayerControler> ().enabled = (false);
+		}
+	}
+	public IEnumerator Fade(){
+		yield return new WaitForSeconds(0.5f);
+		BlackScreen.SetActive (true);
+
+	}
+}
